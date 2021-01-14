5073117350a6b67e492604c96a9dad4ede4aec72
zuzubaidillah <zuzubaidillah@gmail.com>
2021-01-14 16:15:28 +0700

    perubahan file memasukkan tag html

--- footer.js
+++ footer.js
@@ -4,6 +4,9 @@ document.write('\
     <div class="row">\
         <div class="col-12">\
             © 2018 Upcube - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.\
+            © 2018 Upcube - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.\
+            © 2018 Upcube - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.\
+            © 2018 Upcube - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.\
         </div>\
     </div>\
 </div>\
